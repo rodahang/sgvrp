6af026df1af9c575a1e5b7cc7a04625d062b9eb2
Roseele Dahang <rodahang@gmail.com>
2018-06-06 19:33:29 +0800

    added dashboard layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( mdb.min.css )
+
+
+Rails.application.config.assets.precompile += %w( jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( popper.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( mdb.min.js )
+Rails.application.config.assets.precompile += %w( dashboard_layout.css )
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+
+  root 'dashboard#home'
+  get 'team', to: 'dashboard#projects'
+  get 'about', to: 'dashboard#about'
+  
 end
